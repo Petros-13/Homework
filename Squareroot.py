6bbd6d7a0a73494e7e0c795c6286d223d756e6dc
Petros-13 <petroskalkounos78@gmail.com>
2025-07-20 13:04:12 +0200

    V1 homework

---
+++ Squareroot.py
@@ -0,0 +1,11 @@
+import math
+
+print("Welcome to the Square Root Calculator\n")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+sqrt1 = math.sqrt(num1)
+sqrt2 = math.sqrt(num2)
+print(f"\nThe square root of {num1} is {sqrt1}")
+print(f"The square root of {num2} is {sqrt2}")
+
+
