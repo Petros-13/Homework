af9a8e9762b21aa2749f4894c0fa3bf8bee798c0
Petros-13 <petroskalkounos78@gmail.com>
2025-07-25 10:27:29 +0200

    1st commit

---
+++ Alphabet.py
@@ -0,0 +1,5 @@
+char = input("Enter a single character: ")
+if ('a' <= char <= 'z') or ('A' <= char <= 'Z'):
+    print("That is a letter in the alphabet.")
+else:
+    print("That is NOT a letter in the alphabet.")
