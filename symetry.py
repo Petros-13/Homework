71e1ead0b207efa3fab691c038fda5c6052db80e
Petros-13 <petroskalkounos78@gmail.com>
2025-09-21 17:24:01 +0200

    1st  commit

---
+++ symetry.py
@@ -0,0 +1,7 @@
+set1 = {'blue', 'green'}
+set2 = {'blue', 'yellow'}
+print("A:", set1.symmetric_difference(set2))
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {1, 5, 6, 7, 8, 9}
+print("B:", set1.symmetric_difference(set2))
