400be33bf1b087e116da02a14eab5b28abe90fc8
Petros-13 <petroskalkounos78@gmail.com>
2026-02-01 12:16:59 +0100

    1st commit

---
+++ Minimum.py
@@ -0,0 +1,8 @@
+def minimum_flips(arr):
+    count_0 = arr.count(0)
+    count_1 = arr.count(1)
+    return min(count_0, count_1)
+
+# Example usage
+arr = [1, 0, 1, 1, 0]
+print(minimum_flips(arr))
